98315662ecdfe18a60401e7e671a03c322919695
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-07 13:41:29 -0400

    finished tutorial 10

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-var btns = document.querySelectorAll('#book-list .delete');
+const list = document.querySelector('#book-list ul');
 
-Array.from(btns).forEach(function(btn) {
-    btn.addEventListener('click', function(event) {
-        const li = event.target.parentElement;
-        
-        li.parentNode.removeChild(li);
-    });
+list.addEventListener('click', function(e) {
+    if (e.target.className == 'delete') {
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
 });
-
-const link = document.querySelector('#page-banner a');
-link.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log('navigation to ', event.target.textContent, ' was prevented.');
-})
